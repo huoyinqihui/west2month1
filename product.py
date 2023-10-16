555fe4c14f7a8c29f0816f591e4ae646fa358d2c
huoyinqihui <144695119+huoyinqihui@users.noreply.github.com>
2023-10-16 20:38:58 +0800

    Add files via upload

--- product.py
+++ product.py
@@ -15,7 +15,7 @@ class Product:
         print("剩余数量:", self.remainingquantity)
 
     # 计算已售出商品的价值
-    def income(self,soldquantity):
+    def sold(self,soldquantity):
         if soldquantity <= self.remainingquantity:
             value = soldquantity * self.unitprice
             self.remainingquantity -= soldquantity
@@ -24,8 +24,9 @@ class Product:
             print("库存不足！")
 
     # 修改商品信息
-    def setdata(self,productname,unitprice,totalquantity):
+    def setdata(self,id,productname,unitprice,totalquantity):
         self.productname = productname
+        self.productid = id
         self.unitprice = unitprice
         self.totalquantity = totalquantity
         self.remainingquantity = totalquantity
@@ -33,7 +34,7 @@ class Product:
 
 product1 = Product(101,"手机",500,50)
 product1.display()
-print("已售出商品价值:", product1.income(10))
+print("已售出商品价值:", product1.sold(10))
 product1.display()
 product1.setdata("平板电脑",800,30)
 product1.display()
